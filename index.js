4b9a4140e5843f18843e624f0cf841b8ff159407
psirenny <djtorres0@gmail.com>
2014-08-24 00:23:52 -0400

    add support for custom formats

--- index.js
+++ index.js
@@ -2,13 +2,27 @@ var defiler = require('json-defiler');
 
 module.exports = function (options) {
   return function (req, res, next) {
-    defiler(options.dir, function (err, data) {
+    var model = req.getModel();
+    module.exports.load(options, function (err, dict) {
       if (err) return next(err);
-      var model = req.getModel();
       var path = options.path || '$lang.dict';
-      var dict = {strings: data.json};
       model.set(path, dict);
       next();
     });
   };
+};
+
+module.exports.load = function (options, callback) {
+  var opts = {dir: options.dir, split: true};
+  defiler(opts, function (err, data) {
+    if (err) return callback(err);
+    var dict = {messageformat: {}};
+    dict.messageformat.locale = data.js || {};
+    dict.strings = data.json;
+    for (key in dict.messageformat.locale) {
+      var val = dict.messageformat.locale[key];
+      dict.messageformat.locale[key] = val.toString();
+    }
+    callback(null, dict);
+  });
 };
---
+++ test/index.js
@@ -0,0 +1,36 @@
+var lib = require('..');
+var path = require('path');
+var should = require('chai').should();
+
+describe('derby-lang-fs', function () {
+  it('should be a function', function () {
+    lib.should.be.a('function');
+  });
+
+  describe('load', function () {
+    it('should be a function', function () {
+      lib.should.have.property('load');
+      lib.load.should.be.a('function');
+    });
+
+    it('should return a dictionary', function (done) {
+      var opts = {dir: path.join(__dirname, 'fixture')};
+      lib.load(opts, function (err, dict) {
+        (err === null).should.be.true;
+        dict.should.have.property('strings');
+        dict.strings.should.be.an('object');
+        dict.strings.should.have.property('en');
+        dict.strings.should.have.property('es');
+        dict.strings.en.should.eql({foo: 'bar'});
+        dict.strings.es.should.eql({foo: 'qux'});
+        dict.should.have.property('messageformat');
+        dict.messageformat.should.be.an('object');
+        dict.messageformat.should.have.property('locale');
+        dict.messageformat.locale.should.be.an('object');
+        dict.messageformat.locale.should.have.property('en');
+        dict.messageformat.locale.en.should.be.a('string');
+        done();
+      });
+    });
+  });
+});
