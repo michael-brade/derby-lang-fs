81092665c6449f076b70b4687f90df6b88a6fafa
Michael Brade <brade@kde.org>
2015-05-14 20:54:10 +0200

    beautify code

--- index.js
+++ index.js
@@ -1,28 +1,34 @@
 var defiler = require('json-defiler');
 
-module.exports = function (options) {
-  return function (req, res, next) {
-    var model = req.getModel();
-    module.exports.load(options, function (err, dict) {
-      if (err) return next(err);
-      var path = options.path || '$lang.dict';
-      model.set(path, dict);
-      next();
-    });
-  };
+module.exports = function(options) {
+    return function(req, res, next) {
+        var model = req.getModel();
+        module.exports.load(options, function(err, dict) {
+            if (err) return next(err);
+            var path = options.path || '$lang.dict';
+            model.set(path, dict);
+            next();
+        });
+    };
 };
 
-module.exports.load = function (options, callback) {
-  var opts = {dir: options.dir, split: true};
-  defiler(opts, function (err, data) {
-    if (err) return callback(err);
-    var dict = {messageformat: {}};
-    dict.messageformat.locale = data.js || {};
-    dict.strings = data.json;
-    for (key in dict.messageformat.locale) {
-      var val = dict.messageformat.locale[key];
-      dict.messageformat.locale[key] = val.toString();
-    }
-    callback(null, dict);
-  });
+module.exports.load = function(options, callback) {
+    var opts = {
+        dir: options.dir,
+        split: true
+    };
+    defiler(opts, function(err, data) {
+        if (err) return callback(err);
+
+        var dict = {
+            messageformat: {}
+        };
+        dict.messageformat.locale = data.js || {};
+        dict.strings = data.json;
+        for (key in dict.messageformat.locale) {
+            var val = dict.messageformat.locale[key];
+            dict.messageformat.locale[key] = val.toString();
+        }
+        callback(null, dict);
+    });
 };
